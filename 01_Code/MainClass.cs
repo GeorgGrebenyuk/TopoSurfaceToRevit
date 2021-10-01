2a5bdb4680e6ef129d082ddb86ad7ae849425384
Georg Grebenyuk <inj9@yandex.ru>
2021-10-02 00:12:55 +0300

    Update MainClass.cs
    Add private method to get coordinates of Revit's Survey point.
    Add comments to methods
    Add auxilary gouble-coefficient to coordinates (transform from meters in landxml to feets in revit-space)

--- 01_Code/MainClass.cs
+++ 01_Code/MainClass.cs
@@ -21,13 +21,16 @@ namespace CreateTopoTest
 {
     public class MainClass
     {
-       // private static List<XYZ> Topo_Points = new List<XYZ>();
-        //private static List<PolymeshFacet> Topo_Faces = new List<PolymeshFacet>();
         private MainClass () //List<XYZ> Group1, List<PolymeshFacet> Group2
         {
             //this.Topo_P = Group1;
 			//this.Topo_F = Group2;
         }
+        /// <summary>
+        /// This node create a Revit TopographySurface enity by List with 3-dimentiolal Points (XYZ) and List by PolymeshFacet
+        /// </summary>
+        /// <param name="Topo_Points">List with 3D-Points (XYZ type)</param>
+        /// <param name="Topo_Faces">List with Faces of surface (PolymeshFacet type)</param>
         public static void CreateTopo (List<XYZ> Topo_Points, List<PolymeshFacet> Topo_Faces)
 		{
             var RevitDoc = RevitServices.Persistence.DocumentManager.Instance.CurrentDBDocument;
@@ -38,40 +41,79 @@ namespace CreateTopoTest
             trans.Start("Create topo surface");
             Autodesk.Revit.DB.Architecture.TopographySurface.Create(RevitDoc, Topo_Points, Topo_Faces);
             trans.Commit();
-
-
         }
+        private static double BP_X = 0d;
+        private static double BP_Y = 0d;
+        private static double BP_Z = 0d;
+        /// <summary>
+        /// Method to fing and return coordinates of project's internal survey's point
+        /// </summary>
+        private static void GetBasePointCoords ()
+		{
+            var RevitDoc = RevitServices.Persistence.DocumentManager.Instance.CurrentDBDocument;
+            Transaction trans = new Transaction(RevitDoc);
+            trans.Start("Get Base (survey) point coordinates");
+            var basePoint = new FilteredElementCollector(RevitDoc).OfClass(typeof(BasePoint)).Cast<BasePoint>().Where(p => p.IsShared).ToList().FirstOrDefault();
 
+            BP_Y= basePoint.get_Parameter(BuiltInParameter.BASEPOINT_NORTHSOUTH_PARAM).AsDouble();
+            BP_X = basePoint.get_Parameter(BuiltInParameter.BASEPOINT_EASTWEST_PARAM).AsDouble();
+            BP_Z = basePoint.get_Parameter(BuiltInParameter.BASEPOINT_ELEVATION_PARAM).AsDouble();
+            //BasePoint_Angle = basePoint.get_Parameter(BuiltInParameter.BASEPOINT_ANGLETON_PARAM).AsDouble();
+
+            trans.Commit();
+        }
+        /// <summary>
+        /// Node that convert landxml's data to Lists with Points and faces
+        /// </summary>
+        /// <param name="PathToLandxml">Absolute file's path to landxml file</param>
+        /// <param name="UnitsCoeff">Auxilary double-value coefficient (between meters in landxml surface definition to Revit's units). Optionally = 1000 </param>
+        /// <returns></returns>
         [MultiReturn(new[] { "Topo_Points", "Topo_Faces" })]
-        public static Dictionary<string, object> GetPointsAndFacesFromLandxml (string PathToLandxml) //Dictionary <string,object>
+        public static Dictionary<string, object> GetPointsAndFacesFromLandxml (string PathToLandxml, double UnitsCoeff = 1d/0.3048) //Dictionary <string,object>
         {
+            GetBasePointCoords();
+
             XDocument SourceLandXml = XDocument.Load(PathToLandxml);
             System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
             List<XYZ> Topo_Points = new List<XYZ>();
             List<PolymeshFacet> Topo_Faces = new List<PolymeshFacet>();
 
-        //IEnumerable<XYZ> Topo_Points;
-        //IEnumerable<XYZ> Topo_Faces;
-        //List<XYZ> Topo_Points = new List<XYZ>();
-        //List<PolymeshFacet> Topo_Faces = new List<PolymeshFacet>();
-
-        XElement el_Pnts = SourceLandXml.Descendants().Where(a => a.Name.LocalName == "Pnts").First();
+            XElement el_Pnts = SourceLandXml.Descendants().Where(a => a.Name.LocalName == "Pnts").First();
             IEnumerable<XElement> el_PntsCollection = el_Pnts.Elements().Where(a => a.Name.LocalName == "P");
 
             XElement el_Faces = SourceLandXml.Descendants().Where(a => a.Name.LocalName == "Faces").First();
             IEnumerable<XElement> el_FacesCollection = el_Faces.Elements().Where(a => a.Name.LocalName == "F");
 
+            string [] IndexesOdPoints = new string [el_PntsCollection.Count()];
+
+            int i1 = 0;
             foreach (var OnePoint in el_PntsCollection)
 			{
-                double[] PntsCoords = OnePoint.Value.Split(' ').Select(x => double.Parse(x, CultureInfo.GetCultureInfo("en-US"))).ToArray();
-                XYZ new_Point = new XYZ(PntsCoords[1], PntsCoords[0], PntsCoords[2]);
+                double[] PntsCoords = OnePoint.Value.Split(' ').Select(x => double.Parse(x, CultureInfo.GetCultureInfo("en-US")) * UnitsCoeff).ToArray();
+                XYZ new_Point = new XYZ(PntsCoords[1] - BP_X, PntsCoords[0] - BP_Y, PntsCoords[2] - BP_Z);
                 Topo_Points.Add(new_Point);
+                IndexesOdPoints[i1] =  OnePoint.Attribute("id").Value;
+                i1++;
             }
             foreach (var OneFace in el_FacesCollection)
 			{
-                int [] PntsNums = OneFace.Attribute("n").Value.Split(' ').Select(x => int.Parse(x, CultureInfo.GetCultureInfo("en-US"))).ToArray();
-                PolymeshFacet new_Face =  new PolymeshFacet(PntsNums[0], PntsNums[1], PntsNums[2]);
+                int[] PntsNums = OneFace.Value.Split(' ').Select(x => int.Parse(x, CultureInfo.GetCultureInfo("en-US"))).ToArray();
+                PolymeshFacet new_Face =  new PolymeshFacet(GetIndexOfFacetsPoint(PntsNums[0]), GetIndexOfFacetsPoint(PntsNums[1]), GetIndexOfFacetsPoint(PntsNums[2]));
                 Topo_Faces.Add(new_Face);
+            }
+
+            //Auxilay method that return true Point's position by point's id in Face's definition
+            int GetIndexOfFacetsPoint (int CurrentIndexInFace)
+			{
+                for (int i2 = 0; i2 < IndexesOdPoints.Length; i2++)
+				{
+                    if (IndexesOdPoints[i2] == CurrentIndexInFace.ToString())
+					{
+                        return i2;
+					}
+				}
+                return 0;
+
             }
 
 			return new Dictionary<string, object>()
